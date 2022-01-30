fcff4b102bb4d492e49aaeb3cc400027376c4ad5
Sota Ito <>
2022-01-30 21:57:39 +0900

    コミット

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Link from 'next/link'
+import useSWR from 'swr'
 import Header from './components/header'
 import Content from './components/content'
 import Styles from '../styles/Home.module.css'
@@ -7,6 +8,9 @@ import Styles from '../styles/Home.module.css'
 
 export default function Home() {
   let title = 'こんにちは'
+/*   const {data, error} = useSWR('/api/message')
+  if (error) return <div>読み込みに失敗しました。</div>
+  if (!data) return <div>loading...</div> */
 
   return (
     <Content>
@@ -15,7 +19,8 @@ export default function Home() {
       </Head>
       <Header title={ title }/>
 {/*       <h1 className="site-title" style={{ color: `red`, backgroundColor: `#9f9` }}>{ title }</h1> */}
-      <p>学ぶ。をちゃんと</p>
+{/*       <p>{data.message}</p> */}
+      <p>こんにちは</p>
       <div>
         <Link href="/about"><a>リンク</a></Link>
 {/*         <Link href="/about"><button>ボタン</button></Link> */}
